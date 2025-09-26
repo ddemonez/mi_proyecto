aa83f5125278d2f407a148de813d28de94df1d10
ddemonez <esvincalel4@gmail.com>
2025-09-26 15:58:49 -0600

    primer commit

---
+++ src/tallermotos/Cliente.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallermotos;
+public class Cliente {
+    private String idCliente;
+    private String nombre;
+    private String telefono;
+
+    public Cliente(String idCliente, String nombre, String telefono) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public void setIdCliente(String idCliente) { this.idCliente = idCliente; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public void mostrarInfo() {
+        System.out.println("Cliente: " + idCliente + " - " + nombre + " - Tel: " + telefono);
+    }
+}
